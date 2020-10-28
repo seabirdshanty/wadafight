cc1de9fd891de766e2aca470833d6b52b9968517
Byrdie <seabirdshanty@users.noreply.github.com>
2020-10-28 03:23:09 -0700

    Add files via upload

---
+++ fishyDat.js
@@ -0,0 +1,101 @@
+var randFish = "";
+
+var fishNum=[]
+fishNum[0]= "wadanohara"
+fishNum[1]= "samekichi"
+fishNum[2]= "memoca"
+fishNum[3]= "dolphi"
+fishNum[4]= "fukami"
+fishNum[5]= "lobco" 
+fishNum[6]= "chlomaki" 
+fishNum[7]= "sal"
+
+var fish=[]
+fish[0]={
+	firstName:"Wadanohara", 
+	title1:"sea witch", 
+	title2:"apprentice", 
+	hairColor:"brunette", 
+	eyeColor:"brown", 
+	upperClothing:"shirt", 
+	lowerClothing:"skirt", 
+	houseType:"Boat", 
+	imgStr:"wada"
+	}
+fish[1]={
+	firstName:"Samekichi", 
+	title1:"Shark", 
+	title2:"Punk", 
+	hairColor:"deep blue", 
+	eyeColor:"deep blue", 
+	upperClothing:"punk jacket", 
+	lowerClothing:"pants", 
+	houseType:"sea", 
+	imgStr:"same"
+	}
+fish[2]={
+	firstName:"Memoca", 
+	title1:"Pelican", 
+	title2:"Trickster", 
+	hairColor:"white", 
+	eyeColor:"yellow", 
+	upperClothing:"long shirt", 
+	lowerClothing:"pants", 
+	houseType:"Home", 
+	imgStr:"mem"
+	}
+fish[3]={
+	firstName:"Dolphi", 
+	title1:"Dolphin", 
+	title2:"Crybaby", 
+	hairColor:"Blue", 
+	eyeColor:"black", 
+	upperClothing:"sailor dress", 
+	lowerClothing:"sailor dress", 
+	houseType:"Home", 
+	imgStr:"dol"
+	}
+fish[4]={
+	firstName:"Fukami", 
+	title1:"Octopus", 
+	title2:"Mysterious", 
+	hairColor:"pale teal", 
+	eyeColor:"blue", 
+	upperClothing:"long coat", 
+	lowerClothing:"long coat", 
+	houseType:"Home", 
+	imgStr:"fuka"
+	}
+fish[5]={
+	firstName:"Lobco", 
+	title1:"Lobster", 
+	title2:"Familliar", 
+	hairColor:"red", 
+	eyeColor:"red", 
+	upperClothing:"marshall coat", 
+	lowerClothing:"pants", 
+	houseType:"witch academy", 
+	imgStr:"lob"
+	}
+fish[6]={
+	firstName:"Chlomaki", 
+	title1:"Cat Angel", 
+	title2:"Witchy", 
+	hairColor:"black", 
+	eyeColor:"black", 
+	upperClothing:"dress coat", 
+	lowerClothing:"dress coat", 
+	houseType:"witch academy", 
+	imgStr:"chlo"
+	}
+fish[7]={
+	firstName:"Sal", 
+	title1:"Shark", 
+	title2:"Ambassador", 
+	hairColor:"White", 
+	eyeColor:"red", 
+	upperClothing:"suit and tie", 
+	lowerClothing:"pants", 
+	houseType:"Red Sea", 
+	imgStr:"sal"
+	}
